f91f84f94a0e87f01898744052348a03fe01eee0
QAISER-HUSSAIN-01 <merndeveloper01@gmail.com>
2023-06-05 11:49:15 +0500

    test recaptcha

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react'
+import Reaptcha from 'reaptcha'
 
-function App() {
+export default function App() {
+
+  const recaptcha = useRef(null);
+  const verifyCaptcha = () => {
+    recaptcha.current.getResponse().then(res => {
+      console.log(res);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>hello world <br /> <Reaptcha sitekey='6LeoNGwmAAAAAHgoTnB0cXcZE0ikUlkMDlD6VUhx' ref={recaptcha} onVerify={verifyCaptcha} /> </div>
+  )
 }
-
-export default App;
